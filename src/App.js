dd51cf971bf94b4dfe2ba803b166d5a16343e362
blackcat-007 <shubhodeepmukherjee242gmail.com>
2024-08-06 09:31:17 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from "./component/header";
+import Cards from "./component/cards";
+import {Routes,Route} from "react-router-dom";
+import Addmovies from "./component/addmovies";
+import Details from "./component/details";
+import { createContext,useState } from 'react';
+import Login from './component/login';
+import Signup from './component/signup';
+const Appstate=createContext()
 function App() {
+  const[login,setLogin]=useState(false)
+  const[username,setUsername]=useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Appstate.Provider value={{login,username,setLogin,setUsername}}>
+    <div className="App relative">
+      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Cards/>}/>
+        <Route path="/addmovies" element={<Addmovies/>}/>
+        <Route path="/details/:id" element={<Details/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        
+      
+      </Routes>
+      
     </div>
+    </Appstate.Provider>
   );
 }
 
 export default App;
+export {Appstate};
