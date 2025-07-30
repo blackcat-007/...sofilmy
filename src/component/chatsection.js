832cb072bc833f015c5b0fae9264ffed1f17620b
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-07-30 23:48:16 +0530

    chat feature ui update

--- src/component/chatsection.js
+++ src/component/chatsection.js
@@ -31,33 +31,44 @@ const ChatSection = ({ user }) => {
   };
 
   return (
-    <div className="h-[90%] w-full ">
-      <div>
-      <img className="h-[50%] w-full"src="/images/sofilmywallpaper.jpg"/></div>
-      <div className="z-20">
-        
-        <div className="h-[90%] z-20 w-full">
-        {messages.map((msg) => (
-          <div key={msg.id}>
-            <img src={msg.data.photoURL || ""} alt="User avatar" />
-            {msg.data.text}
-          </div>
-        ))}
-      </div >
-    <div className="w-full flex justify-center z-20 ">
+   <div className="relative h-screen   w-full *:flex flex-col items-center justify-center bg-black text-white">
+  {/* Background Image */}
+  <img
+    className="absolute top-0 left-0 w-full h-full object-cover z-0"
+    src="/images/sofilmywallpaper.jpg"
+    alt="Background"
+  />
+
+  {/* Overlay for messages */}
+  <div className="relative z-0 flex flex-col justify-between h-full w-full">
+    {/* Message List */}
+    <div className="overflow-y-auto p-4 flex-1">
+      {messages.map((msg) => (
+        <div key={msg.id} className="mb-2 bg-white bg-opacity-70 p-2 rounded-md max-w-[70%]">
+          <img src={msg.data.photoURL || ""} alt="User avatar" className="w-6 h-6 rounded-full inline-block mr-2" />
+          <span>{msg.data.text}</span>
+        </div>
+      ))}
+    </div>
+
+    {/* Input Area Fixed at Bottom */}
+    <div className="w-full px-4 py-2 bg-white bg-opacity-80 flex items-center justify-center sticky bottom-0 z-10">
       <input
-        className="bg-white  text-black h-10  w-[50%] border rounded-2xl"
+        className="bg-white text-black h-10 w-[60%] border rounded-2xl px-4"
         value={newMessage}
         onChange={(e) => setNewMessage(e.target.value)}
         placeholder="Type a message"
       />
-      <button className="text-white  bg-green-600 w-16 h-10 border rounded-3xl" onClick={sendMessage}>
+      <button
+        className="ml-2 text-white bg-green-600 w-16 h-10 border rounded-3xl"
+        onClick={sendMessage}
+      >
         Send
       </button>
-      </div> 
-      
-      </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
