a30c49bbc06067fe364e822717d13b4805a4ad00
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-08-09 00:00:08 +0530

    ui updated

--- src/component/analysis.js
+++ src/component/analysis.js
@@ -32,21 +32,45 @@ function Analysis() {
   ) : (
     datas.map((e, i) => (
       <Link to={`/details/${e.id}`} key={i}>
-        <div className="card p-4 bg-white rounded shadow-md transition-transform duration-300 transform hover:translate-y-2 hover:scale-105 hover:shadow-red-500 h-full">
-         <div className='flex justify-center'> <img className="w-56 h-72 object-cover mb-3 rounded" src={e.image} alt={e.name} /></div>
-          <h1><span className="text-red-400 mr-1">Name:</span>{e.name}</h1>
-          <h2 className="flex items-center">
-            <span className="text-red-400 mr-2">Rating:</span>
-            <div className="flex items-center">
-              <ReactStars size={20} half={true} value={e.rating / e.user} edit={false} />
-            </div>
-             
-          </h2>
-           <p className="text-gray-500 truncate overflow-hidden whitespace-nowrap">
-  {e.description}
-</p>
-          <h3><span className="text-red-400 mr-2">Year:</span>{e.year}</h3>
-        </div>
+       <div className="card p-4 bg-white rounded shadow-md transition-transform duration-300 transform hover:translate-y-2 hover:scale-105 hover:shadow-red-500 h-full">
+  <div className='flex justify-center'>
+    <img
+      className="w-56 h-72 object-cover mb-3 rounded"
+      src={e.image}
+      alt={e.name}
+    />
+  </div>
+
+  {/* Green dashed divider */}
+  <div className="border-t-2 border-green-500 border-dashed my-3"></div>
+
+  <h1>
+    <span className="text-red-400 mr-1">Name:</span>
+    {e.name}
+  </h1>
+
+  <h2 className="flex items-center">
+    <span className="text-red-400 mr-2">Rating:</span>
+    <div className="flex items-center">
+      <ReactStars
+        size={20}
+        half={true}
+        value={e.rating / e.user}
+        edit={false}
+      />
+    </div>
+  </h2>
+
+  <p className="text-gray-500 truncate overflow-hidden whitespace-nowrap">
+    {e.description}
+  </p>
+
+  <h3>
+    <span className="text-red-400 mr-2">Year:</span>
+    {e.year}
+  </h3>
+</div>
+
       </Link>
     ))
   )}
